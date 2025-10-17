277d3eff0b42c7a2a345c4db354ab850c9722e2c
Rahul Rajput <rahulrajput808160@gmail.com>
2025-10-18 02:44:06 +0530

    Connected MongoDB + tested API routes

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+
+
+
+const signup = (req, res) => {
+    res.send("Hello this is auth World");
+};
+
+const login = (req, res) => {
+    res.send("Hello this is auth World");
+};
+
+const logout = (req, res) => {
+    res.send("Hello this is auth World");
+};
+
+export { signup, login, logout };
