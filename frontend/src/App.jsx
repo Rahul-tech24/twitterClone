62af234940e3143bd118de39e011c2ceff0616b4
Rahul Rajput <rahulrajput808160@gmail.com>
2025-10-19 14:04:41 +0530

    feat: add React frontend with auth, posts, profiles, and Tailwind UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
 
+import { Routes } from 'react-router-dom';
 import './App.css'
+import HomePage from './pages/home/HomePage.jsx';
+import LoginPage from './pages/auth/login/LoginPage.jsx';
+import SignupPage from './pages/auth/signup/SignUpPage.jsx';
+import { Route } from 'react-router-dom';
+import Sidebar from './components/common/Sidebar.jsx';
+import RightPanel from './components/common/RightPanel.jsx';
+import ProfilePage from './pages/profile/ProfilePage.jsx';
+
 
 function App() {
  
   return (
     <>
-      frontend initial commit
-       
+      <div className="flex max-w-6xl mx-auto">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/profile/:username" element={<ProfilePage />} />
+        </Routes>
+        <RightPanel />
+      </div>
+         
     </>
   )
 }
